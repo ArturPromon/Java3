2fe50c10bbd1a6f8eabb747a5de0857f926fcfd2
ArturPromon <artur.shovkunenko@gmail.com>
2022-02-14 20:03:27 +0200

    for in for

--- src/com/company/Main8.java
+++ src/com/company/Main8.java
@@ -2,6 +2,11 @@ package com.company;
 
 public class Main8 {
     public static void main(String[] args){
-
+        for(int j = 1; j<3;j++ ) {
+            System.out.println();
+            for (int i = 1; i < 11; i++) {
+                System.out.print(" "+i*j + "\t");
+            }
+        }
     }
 }
